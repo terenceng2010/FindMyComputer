9f32507e2ba58374c42c91930f0d3e14e586a270
terenceng2010 <terenceng2010@gmail.com>
2021-05-09 16:57:30 +0100

    Update ComputerRecommendation.js

--- FindMyComputer/wwwroot/js/components/ComputerRecommendation.js
+++ FindMyComputer/wwwroot/js/components/ComputerRecommendation.js
@@ -41,22 +41,26 @@ export default {
                 </template>          
                 <template v-slot:content>
                     <div class="uk-column-1-2 uk-column-divider">
-                        <p><span class="uk-label">RAM</span> {{c.ram}}MB</p>
+                        <p><span class="uk-label">RAM</span>
+                           <span class="uk-margin-left" v-if='c.ram < 1000'>{{c.ram}}MB</span>
+                           <span class="uk-margin-left" v-else>{{c.ram/1000}}GB</span>
+                        </p>
                         <p>
                             <span class="uk-label uk-label-danger" v-if="query=='HarddiskSize'">HARDDISK</span>
                             <span class="uk-label" v-else>HARDDISK</span>
-                            {{c.harddiskSize}}GB {{c.harddiskType}}
+                            <span class="uk-margin-left" v-if='c.harddiskSize < 1000'>{{c.harddiskSize}}GB {{c.harddiskType}}</span>
+                            <span class="uk-margin-left" v-else>{{c.harddiskSize/1000}}TB {{c.harddiskType}}</span>
                         </p>
                         <p>
-                            <span class="uk-label uk-label-danger" v-if="query=='ConnectorCount'">CONNECTIVITY</span>
-                            <span class="uk-label" v-else>CONNECTIVITY</span> 
+                            <span class="uk-label uk-label-danger" v-if="query=='ConnectorCount'">CONNECTIVITY({{c.connectorCount}})</span>
+                            <span class="uk-label" v-else>CONNECTIVITY({{c.connectorCount}})</span> 
                             <span class="uk-margin-left" v-for="conn in c.connectors">{{conn.name}} x {{conn.quantity}}</span>
                         </p>
                         <p><span class="uk-label">GPU</span> {{c.graphicsCardModel}}</p>
                         <p>
                             <span class="uk-label uk-label-danger" v-if="query=='TowerWeight'">WEIGHT</span>
                             <span class="uk-label" v-else>WEIGHT</span>
-                            {{c.towerWeight}}KG
+                            {{c.towerWeight}}KG ({{ Math.round(c.towerWeight*2.20462262*10)/10}}LB)
                         </p>
                         <p><span class="uk-label">POWER SUPPLY</span> {{c.powerSupplyWatt}}W</p>  
                         <p><span class="uk-label">CPU</span> {{c.cpuModel}}</p>
