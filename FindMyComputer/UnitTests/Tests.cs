a6a51add4ab3fd35c67e39e87eb38d689958cff7
terenceng2010 <terenceng2010@gmail.com>
2021-05-10 01:00:13 +0100

    add nunit

---
+++ FindMyComputer/UnitTests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindMyComputer.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ReadMemorySizeText()
+        {
+            decimal gb = FindMyComputer.Manager.ComputerManagerUtility.ReadRam("500 GB");
+            Assert.AreEqual(500000, gb);
+
+            decimal mb = FindMyComputer.Manager.ComputerManagerUtility.ReadRam("500000 MB");
+            Assert.AreEqual(500000, mb);
+        }
+    }
+}
