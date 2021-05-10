adcd52f7d0ea821e8faeafdffc837bcb342d8afb
terenceng2010 <terenceng2010@gmail.com>
2021-05-10 08:41:58 +0100

    move Filtering methods to utility class

--- FindMyComputer/Manager/ComputerManagerUtility.cs
+++ FindMyComputer/Manager/ComputerManagerUtility.cs
@@ -1,6 +1,10 @@
 ﻿using FindMyComputer.Models;
+using FindMyComputer.ViewModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace FindMyComputer.Manager
@@ -165,5 +169,192 @@ namespace FindMyComputer.Manager
         }
 
 
+        public static IQueryable<Computer> Sort(IQueryable<Computer> computers, string key, bool isDesc, int limit)
+        {
+            Expression<Func<Computer, object>> sortExpression;
+            switch (key)
+            {
+                case "HarddiskSize":
+                    sortExpression = (q => q.HarddiskSize);
+                    break;
+                case "ConnectorCount":
+                    sortExpression = (q => q.ConnectorCount);
+                    break;
+                case "TowerWeight":
+                    sortExpression = (q => q.TowerWeight);
+                    break;
+                default:
+                    sortExpression = (q => q.ComputerId);
+                    break;
+            }
+
+            // sort direction
+            computers = isDesc
+                      ? computers.OrderByDescending(sortExpression)
+                      : computers.OrderBy(sortExpression);
+
+            if (limit > 0)
+            {
+                computers = computers.Take(limit);
+            }
+
+            return computers;
+        }
+
+
+        public static IQueryable<Computer> Filter(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            computers = FilterByRam(computers, vm);
+            computers = FilterByHarddiskSize(computers, vm);
+            computers = FilterByHarddiskType(computers, vm);
+            computers = FilterByGraphicsCardModelKeyword(computers, vm);
+            computers = FilterByTowerWeight(computers, vm);
+            computers = FilterByPowerSupplyWatt(computers, vm);
+            computers = FilterByCPUBrand(computers, vm);
+            computers = FilterByCPUModelKeyword(computers, vm);
+            computers = FilterByComputerIds(computers, vm);
+            return computers;
+        }
+
+
+        //might also opt for reflection with LINQ to abstract common logic in the below filter by methods
+        #region filterByKeyword
+        
+        private static IQueryable<Computer> FilterByGraphicsCardModelKeyword(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (!string.IsNullOrWhiteSpace(vm.GraphicsCardModelKeyword))
+            {
+                //Full-text search should be used instead in a real app, as using % in prefix means a table scan has to be done.
+                computers = computers.Where(c => EF.Functions.Like(c.GraphicsCardModel, "%" + vm.GraphicsCardModelKeyword + "%"));
+            }
+
+            return computers;
+        }
+        private static IQueryable<Computer> FilterByCPUModelKeyword(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (!string.IsNullOrWhiteSpace(vm.CPUModelKeyword))
+            {
+                //Full-text search should be used instead in a real app, as using % in prefix means a table scan has to be done.
+                computers = computers.Where(c => EF.Functions.Like(c.CPUModel, "%" + vm.CPUModelKeyword + "%"));
+            }
+
+            return computers;
+        }
+        #endregion
+        #region filterByMinMaxSize
+        private static IQueryable<Computer> FilterByHarddiskSize(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.MinHarddiskSize != null || vm.MaxHarddiskSize != null)
+            {
+                if (vm.MinHarddiskSize != null && vm.MaxHarddiskSize != null)
+                {
+                    computers = computers.Where(c => c.HarddiskSize >= vm.MinHarddiskSize && c.HarddiskSize <= vm.MaxHarddiskSize);
+                }
+                else if (vm.MinHarddiskSize != null)
+                {
+                    computers = computers.Where(c => c.HarddiskSize >= vm.MinHarddiskSize);
+                }
+                else if (vm.MaxHarddiskSize != null)
+                {
+                    computers = computers.Where(c => c.HarddiskSize <= vm.MaxHarddiskSize);
+                }
+            }
+
+            return computers;
+        }
+
+        private static IQueryable<Computer> FilterByRam(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.MinRam != null || vm.MaxRam != null)
+            {
+                if (vm.MinRam != null && vm.MaxRam != null)
+                {
+                    computers = computers.Where(c => c.Ram >= vm.MinRam && c.Ram <= vm.MaxRam);
+                }
+                else if (vm.MinRam != null)
+                {
+                    computers = computers.Where(c => c.Ram >= vm.MinRam);
+                }
+                else if (vm.MaxRam != null)
+                {
+                    computers = computers.Where(c => c.Ram <= vm.MaxRam);
+                }
+            }
+
+            return computers;
+        }
+
+        private static IQueryable<Computer> FilterByTowerWeight(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.MinTowerWeight != null || vm.MaxTowerWeight != null)
+            {
+                if (vm.MinTowerWeight != null && vm.MaxTowerWeight != null)
+                {
+                    computers = computers.Where(c => c.TowerWeight >= vm.MinTowerWeight && c.TowerWeight <= vm.MaxTowerWeight);
+                }
+                else if (vm.MinTowerWeight != null)
+                {
+                    computers = computers.Where(c => c.TowerWeight >= vm.MinTowerWeight);
+                }
+                else if (vm.MaxTowerWeight != null)
+                {
+                    computers = computers.Where(c => c.TowerWeight <= vm.MaxTowerWeight);
+                }
+            }
+
+            return computers;
+        }
+
+        private static IQueryable<Computer> FilterByPowerSupplyWatt(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.MinPowerSupplyWatt != null || vm.MaxPowerSupplyWatt != null)
+            {
+                if (vm.MinPowerSupplyWatt != null && vm.MaxPowerSupplyWatt != null)
+                {
+                    computers = computers.Where(c => c.PowerSupplyWatt >= vm.MinPowerSupplyWatt && c.PowerSupplyWatt <= vm.MaxPowerSupplyWatt);
+                }
+                else if (vm.MinPowerSupplyWatt != null)
+                {
+                    computers = computers.Where(c => c.PowerSupplyWatt >= vm.MinPowerSupplyWatt);
+                }
+                else if (vm.MaxPowerSupplyWatt != null)
+                {
+                    computers = computers.Where(c => c.PowerSupplyWatt <= vm.MaxPowerSupplyWatt);
+                }
+            }
+
+            return computers;
+        }
+        #endregion
+        #region filterByInClause
+        private static IQueryable<Computer> FilterByCPUBrand(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.CPUBrandList != null && vm.CPUBrandList.Count > 0)
+            {
+                computers = computers.Where(c => vm.CPUBrandList.Contains(c.CPUBrand));
+            }
+
+            return computers;
+        }
+        private static IQueryable<Computer> FilterByHarddiskType(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.HarddiskTypes != null && vm.HarddiskTypes.Count > 0)
+            {
+                computers = computers.Where(c => vm.HarddiskTypes.Contains(c.HarddiskType));
+            }
+
+            return computers;
+        }
+
+        private static IQueryable<Computer> FilterByComputerIds(IQueryable<Computer> computers, ComputerFacetSearchViewModel vm)
+        {
+            if (vm.ComputerIds != null)
+            {
+                computers = computers.Where(c => vm.ComputerIds.Contains(c.ComputerId));
+            }
+
+            return computers;
+        }
+        #endregion
     }
 }
